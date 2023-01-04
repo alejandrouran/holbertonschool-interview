ba017caf85d46ca983dffb8e04647b8f7a3b211e
alejuran <alejandrourn@gmail.com>
2023-01-04 11:50:18 -0500

    update minoperations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """
-calculates the fewest number of operations needed to result in exactly n H characters in the file.
+calculates the fewest number of operations needed
+to result in exactly n H characters in the file.
 """
+
+
 def minOperations(n):
     numOpe = 0
     minOpe = 2
