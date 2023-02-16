0d28133013bc291f1ad6d9fc9c8b878d937ba0d4
alejuran <alejandrourn@gmail.com>
2023-02-16 16:58:57 -0500

    update validate

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -3,14 +3,14 @@
 
 
 def validUTF8(data):
-    """method that determines if a 
-    given data set represents a 
+    """method that determines if a
+    given data set represents a
        valid UTF-8 encoding"""
 
     if len(data) == 0:
         return True
     if len(data) == 1:
-        if  data[0] >= 32 and data[0] <= 126:
+        if data[0] >= 32 and data[0] <= 126:
             return True
         else:
             return False
