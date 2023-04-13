943eaff4bb325ca96cbe707534ee3010f8e0127b
alejuran <alejandrourn@gmail.com>
2023-04-13 16:52:10 -0500

    all stars wars api

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieNum = process.argv[2];
+const movieUrl = `https://swapi.dev/api/films/${movieNum}/`;
+request(movieUrl, async function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(character, (error, res, html) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
+      });
+      console.log(res);
+    }
+  }
+});
