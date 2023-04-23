46cfa82d97a83ac2150462a38c4d380218c0d68f
Alejandro Uran <alejandrourn@gmail.com>
2023-04-23 18:07:57 -0500

    all rain

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Given a list of non-negative integers representing the heights of walls with unit width 1
+"""
+
+def rain(walls):
+    """
+    walls is a list of non-negative integers.
+    Return: Integer indicating total amount of rainwater retained.
+    """
+    n = len(walls)
+    total_water = 0
+
+    for i in range(1, n - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+
+        for j in range(i + 1, n):
+            right = max(right, walls[j])
+
+        total_water = total_water + (min(left, right) - walls[i])
+    return total_water
