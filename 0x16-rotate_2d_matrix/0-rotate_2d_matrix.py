9c3d2895fad66e9c4e2378ffa18ddbf385102a62
Alejandro Uran <alejandrourn@gmail.com>
2023-05-31 17:07:11 -0500

    update task one rotate matrix

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,12 +3,11 @@
 
 
 def rotate_2d_matrix(matrix):
-    """ Rotate a 2D matrix """
     n = len(matrix)
 
     for i in range(n):
         for j in range(i + 1, n):
-            matrix[1][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
     for i in range(n):
         matrix[i] = matrix[i][::-1]
