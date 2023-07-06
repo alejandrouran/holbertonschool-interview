0ab0a3c8871408f74ce78aa4152883e9e9feb4d2
Alejandro Uran <alejandrourn@gmail.com>
2023-07-06 15:22:47 -0500

    update making change

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -10,17 +10,15 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    coins.sort(reverse=True)
-    sum = 0
-    i = 0
-    counter = 0
-    num_coins = len(coins)
-    while sum < total and i < num_coins:
-        while coins[i] <= total - sum:
-            sum += coins[i]
-            counter += 1
-            if sum == total:
-                return counter
-        i += 1
-    return -1
+    coins_required = [float('inf')] * (total + 1)
+    coins_required[0] = 0
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                coins_required[i] = min(coins_required[i],
+                                        coins_required[i - coin] + 1)
 
+    if coins_required[total] == float('inf'):
+        return -1
+
+    return coins_required[total]
