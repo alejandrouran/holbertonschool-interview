ca545e84c2ebd03e80ebbba26c45085a419ef446
Alejandro Uran <alejandrourn@gmail.com>
2023-07-06 19:42:24 -0500

    update making change

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -4,15 +4,15 @@
 
 def makeChange(coins, total):
     """ Given a pile of coins of different values """
-    if total <= 0:
+    temp_value = 0
+    coins.sort(reverse=True)
+
+    if total < 0:
         return 0
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-    coins.sort()
-    for i in range(total + 1):
-        for coin in coins:
-            if coin > i:
-                break
-            if dp[i - coin] != -1:
-                dp[i] = min(dp[i - coin] + 1, dp[i])
-    return dp[total] if dp[total] != float('inf') else -1
+
+    for coin in coins:
+        if total % coin <= total:
+            temp_value += total // coin
+            total = total % coin
+
+    return temp_value if total == 0 else -1
