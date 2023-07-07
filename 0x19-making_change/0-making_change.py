58281d0264efcd3a802c4e3ba4a9eba906da9f03
Alejandro Uran <alejandrourn@gmail.com>
2023-07-06 19:22:24 -0500

    update making change

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -15,9 +15,15 @@ def makeChange(coins, total):
     if total < 0:
         return 0
 
-    for coin in coins:
-        if total % coin <= total:
-            temp_value += total // coin
-            total = total % coin
+    coins_required = [float('inf')] * (total + 1)
+    coins_required[0] = 0
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                coins_required[i] = min(coins_required[i],
+                                        coins_required[i - coin] + 1)
 
-    return temp_value if total == 0 else -1
+    if coins_required[total] == float('inf'):
+        return -1
+
+    return coins_required[total]
