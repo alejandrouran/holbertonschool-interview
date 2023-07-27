1247d1f11b34895057b9f283070527b874336764
Alejandro Uran <alejandrourn@gmail.com>
2023-07-27 12:18:28 -0500

    update island perimeter

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -3,7 +3,11 @@
 Island Perimeter
 """
 
+
 def island_perimeter(grid):
+    """
+    returns the perimeter of the island
+    """
     perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
@@ -14,7 +18,7 @@ def island_perimeter(grid):
                 perimeter += 4
 
                 if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 2 
+                    perimeter -= 2
                 if col > 0 and grid[row][col - 1] == 1:
                     perimeter -= 2
 
